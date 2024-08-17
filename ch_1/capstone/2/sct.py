0454c0edd24689d70be4f65ab61593f4316b51f7
BroaderImpact <51309854+BroaderImpact@users.noreply.github.com>
2024-08-17 03:47:32 -0400

    Update sct.py

--- ch_1/capstone/2/sct.py
+++ ch_1/capstone/2/sct.py
@@ -3,3 +3,5 @@ tensor1 = torch.tensor([1, 2, 3])
 tensor2 = torch.tensor([4, 5, 6])
 result = tensor1 + tensor2
 assert torch.equal(result, torch.tensor([5, 7, 9])), "Step 2 failed: Incorrect result for PyTorch element-wise addition."
+
+# Ex().success_msg("All checks have passed.")

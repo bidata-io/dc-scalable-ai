40e8762812e40890ae75cb02d139ddb77ff49e81
BroaderImpact <51309854+BroaderImpact@users.noreply.github.com>
2024-08-17 04:57:06 -0400

    Create sct.py

---
+++ ch_3/capstone/4/sct.py
@@ -0,0 +1,4 @@
+correct_answer = "FastAPI"  # Or "MLflow" depending on the expected use case.
+assert selected_answer == correct_answer, f"The correct answer is {correct_answer}."
+
+Ex().success_msg("All checks passed successfully.")
